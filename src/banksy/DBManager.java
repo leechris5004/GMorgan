a00afd0e471372f490771d2420697cd3cf7f5abe
Khayyon1 <User@6CG4292R9M>
2019-12-09 12:12:04 -0500

    Added Create Database Function in DBManager and added some db creation code to Main.java

---
+++ src/banksy/DBManager.java
@@ -0,0 +1,26 @@
+package banksy;
+
+import java.sql.Connection;  
+import java.sql.DatabaseMetaData;  
+import java.sql.DriverManager;  
+import java.sql.SQLException; 
+
+public class DBManager
+{
+	public static void createNewDatabase(String fileName) 
+	{
+		String url =  "jdbc:sqlite:~\\eclipse-workspace\\GMorgan\\src" + fileName;
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			if(conn != null) 
+			{
+				DatabaseMetaData meta = conn.getMetaData();
+				System.out.println("The driver name is "+meta.getDriverName());
+				System.out.println("A new Database has been created at "+url);
+			}
+		}catch(SQLException e) 
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
--- src/banksy/Main.java
+++ src/banksy/Main.java
@@ -2,10 +2,10 @@ package banksy;
 
 public class Main {
 
-	public static void main(String[] args) {
-
-
-
+	public static void main(String[] args) 
+	{
+		DBManager db = new DBManager();
+		db.createNewDatabase("Banksy.db");
 	}
 
 }
