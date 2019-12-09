67cdfcc2bbcf89a24edcbad23683d1f0ad310853
Khayyon1 <k_parker@coloradocollege.edu>
2019-12-09 12:35:25 -0500

    Tried Phil's System Properties

--- src/banksy/DBManager.java
+++ src/banksy/DBManager.java
@@ -6,18 +6,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException; 
 
 public class DBManager
-//merge conflicts
+//merge conflictsa
 {
 	public static void createNewDatabase(String fileName) 
 	{
-		String url =  "jdbc:sqlite:~\\eclipse-workspace\\GMorgan\\src" + fileName;
+		//String url =  "jdbc:sqlite:~\\eclipse-workspace\\GMorgan\\src" + fileName;
+		String urlpath = System.getProperty("user.dir")+"\\src\\banksy";
+		System.out.println(urlpath);
 		try {
-			Connection conn = DriverManager.getConnection(url);
+			Connection conn = DriverManager.getConnection(urlpath);
 			if(conn != null) 
 			{
 				DatabaseMetaData meta = conn.getMetaData();
 				System.out.println("The driver name is "+meta.getDriverName());
-				System.out.println("A new Database has been created at "+url);
+				System.out.println("A new Database has been created at "+urlpath);
 			}
 		}catch(SQLException e) 
 		{
