ee6da75ccf485052827ddc504a447010bc58c910
Unknown <christianlazy@gmail.com>
2019-12-09 13:57:32 -0500

    Created Transaction class.

---
+++ src/banksy/Transaction.java
@@ -0,0 +1,11 @@
+package banksy;
+
+/* Transaction is called by main to manage transactions.
+In the future, it may have concurrency capabilities.
+ */
+public class Transaction {
+
+    public void CreateTransaction(int userID, int accountID, int amount) {
+
+    }
+}
