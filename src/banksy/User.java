55e13be172c5aa9e6e20aca0a01c398a3326d44f
gabe <gabrielmorgan1230@gmail.com>
2019-12-09 11:45:22 -0500

    initial structure, four files.

--- src/banksy/User.java
+++ src/banksy/User.java
@@ -1,1 +1,27 @@
 package banksy;
+
+
+
+public class User{
+
+public boolean createUser(){
+  return false;
+}
+
+private boolean nameCheck(){
+  return false;
+}
+
+private boolean passwordCheck(){
+  return false;
+}
+
+private boolean emailCheck(){
+  return false;
+}
+
+private boolean ssnCheck(){
+  return false;
+}
+
+}
