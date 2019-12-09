d86802feef1ca2994c27042b2cada6c4210e33e8
Unknown <christianlazy@gmail.com>
2019-12-09 13:42:29 -0500

    Cleaned up errors in Main

--- src/banksy/Main.java
+++ src/banksy/Main.java
@@ -2,10 +2,8 @@ package banksy;
 
 public class Main {
 
-	public static void main(String[] args) 
-	{
-		DBManager db = new DBManager();
-		db.createNewDatabase("Banksy.db");
+	public static void main(String[] args) {
+		// TODO:
 	}
 
 }
