936413eb14492fca3eb31890b5d99a6778d6b003
Unknown <christianlazy@gmail.com>
2019-12-09 14:03:50 -0500

    Minor format changes to User

--- src/banksy/User.java
+++ src/banksy/User.java
@@ -4,81 +4,83 @@ import java.util.regex.Pattern;
 import java.security.MessageDigest;
 import java.nio.charset.StandardCharsets;
 
+/* User performs checks on the user input to create a valid User.
+*/
 public class User {
+    public String firstName;
+    public String lastName;
 
-public String firstName;
-public String lastName;
-
-public boolean createUser(){
-        return false;
-}
+    public boolean createUser() {
+            return false;
+    }
 
 
-public boolean isAlpha(String input) {
-        String alphaRegex = "/^[A-Za-z]+$/";
-        Pattern pat = Pattern.compile(alphaRegex);
-        if (input == null)
-                return false;
-        return pat.matcher(input).matches();
-}
+    public boolean isAlpha(String input) {
+            String alphaRegex = "/^[A-Za-z]+$/";
+            Pattern pat = Pattern.compile(alphaRegex);
+            if (input == null)
+                    return false;
+            return pat.matcher(input).matches();
+    }
 
-private boolean nameCheck(String fname, String lname){
-        // Must be alphabetic, no numbers, no special characters
-        if(isAlpha(fname)) {
-                this.firstName = fname;
-        }else{
-                return false;
-        }
-        if(isAlpha(lname)) {
-                this.lastName = lname;
-        }else{
-                return false;
-        }
-        return true;
-}
+    private boolean nameCheck(String fname, String lname) {
+            // Must be alphabetic, no numbers, no special characters
+            if(isAlpha(fname)) {
+                    this.firstName = fname;
+            }else{
+                    return false;
+            }
+            if(isAlpha(lname)) {
+                    this.lastName = lname;
+            }else{
+                    return false;
+            }
+            return true;
+    }
 
-private boolean passwordCheck(String passwordCheck){
-        //password must have an uppercase, a lowercase, a number and a symbol.
-        String passwordRegex =  "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)";
+    private boolean passwordCheck(String passwordCheck) {
+            //password must have an uppercase, a lowercase, a number and a symbol.
+            String passwordRegex =  "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)";
 
-        Pattern pat = Pattern.compile(passwordRegex);
-        if (passwordCheck == null)
-                return false;
-        return pat.matcher(passwordCheck).matches();
-}
+            Pattern pat = Pattern.compile(passwordRegex);
+            if (passwordCheck == null)
+                    return false;
+            return pat.matcher(passwordCheck).matches();
+    }
 
-private byte[] hashit(String plaintext){
-  MessageDigest digest = MessageDigest.getInstance("SHA-256");
-  byte[] encodedhash = digest.digest(
-  plaintext.getBytes(StandardCharsets.UTF_8));
+    private byte[] hashit(String plaintext) {
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      byte[] encodedhash = digest.digest(
+      plaintext.getBytes(StandardCharsets.UTF_8));
 
-  return encodedhash;
-}
+      return encodedhash;
+    }
 
-private boolean emailCheck(String email){
-        //regex that i found on google.
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-                            "[a-zA-Z0-9_+&*-]+)*@" +
-                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                            "A-Z]{2,7}$";
+    private boolean emailCheck(String email) {
+            //regex that i found on google.
+            String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                                "[a-zA-Z0-9_+&*-]+)*@" +
+                                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                                "A-Z]{2,7}$";
 
-        Pattern pat = Pattern.compile(emailRegex);
-        if (email == null)
-                return false;
-        return pat.matcher(email).matches();
-}
+            Pattern pat = Pattern.compile(emailRegex);
+            if (email == null)
+                    return false;
+            return pat.matcher(email).matches();
+    }
 
-private boolean ssnCheck(String ssn){
+    private boolean ssnCheck(String ssn) {
 
-        String lengthregex = "^(\\w{9})$";
+            String lengthregex = "^(\\w{9})$";
 
-        Pattern pat = Pattern.compile(lengthregex);
-        if (ssn == null)
-                return false;
-        return pat.matcher(ssn).matches();
-}
+            Pattern pat = Pattern.compile(lengthregex);
+            if (ssn == null)
+                    return false;
+            return pat.matcher(ssn).matches();
+    }
 
-private boolean checksPassed(){
-        return false;
-}
+    private boolean checksPassed() {
+            return false;
+    }
+
 }
