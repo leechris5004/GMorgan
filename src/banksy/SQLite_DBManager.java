20d13586976c4c8d6a83510106c478c7661925ac
Khayyon <k_parker@coloradocollege.edu>
2019-12-09 13:54:47 -0500

    Added SQLite_DBManager to the project

---
+++ src/banksy/SQLite_DBManager.java
@@ -0,0 +1,13 @@
+package banksy;
+
+import java.io.IOException;
+
+public class SQLite_DBManager implements DBManager {
+
+	@Override
+	public void createNewDatabase(String fileName) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
