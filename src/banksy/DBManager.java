c6a8079c467af715022ca6f3d347b6257b3bde36
Unknown <christianlazy@gmail.com>
2019-12-10 10:00:43 -0500

    Converted project into Maven

--- src/banksy/DBManager.java
+++
@@ -1,9 +0,0 @@
-package banksy;
-
-import java.io.IOException;
-
-public interface DBManager
-{
-	public void createNewDatabase(String fileName) throws IOException;
-	public void createNewTable(String fileName, String sql) throws IOException;
-}
---
+++ src/main/java/banksy/DBManager.java
@@ -0,0 +1,9 @@
+package banksy;
+
+import java.io.IOException;
+
+public interface DBManager
+{
+	public void createNewDatabase(String fileName) throws IOException;
+	public void createNewTable(String fileName, String sql) throws IOException;
+}
