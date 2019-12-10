0ce71e12b9b6ea515136e52ae1b71f7ff9229bff
ryu <ryukasun@gmail.com>
2019-12-10 10:33:45 -0500

    Starting to edit transaction

--- src/main/java/banksy/Transaction.java
+++ src/main/java/banksy/Transaction.java
@@ -1,11 +1,34 @@
 package banksy;
 
+import java.sql.Timestamp;
+
 /* Transaction is called by main to manage transactions.
 In the future, it may have concurrency capabilities.
  */
 public class Transaction {
 
+    protected int userID;
+    protected int accountID;
+    protected int amount; //Positive amount means deposit, Negative amount means withdraw of some sort
+    protected int otherAccountID; //NULL default, given a value if sending or taking money from account
+    protected String depositType; //
+    protected Timestamp TIMESTAMP;
+
+
+
     public void CreateTransaction(int userID, int accountID, int amount) {
+        //Active Transactions
+
+    }
+
+    public void checkType(){
+        if(this.amount > 0){
+            this.depositType = "Deposit";
+        }
+        else{
+            this.depositType = "Withdraw";
+        }
+        
 
     }
 }
