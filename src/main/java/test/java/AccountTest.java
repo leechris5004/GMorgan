0b3ddd23ec946091de23a5f3c7624b3ea88cf4a4
ryu <ryukasun@gmail.com>
2019-12-11 14:45:51 -0500

    Simply adding in some test for the Account object

---
+++ src/main/java/test/java/AccountTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import banksy.Account;
+
+public class AccountTest {
+}
