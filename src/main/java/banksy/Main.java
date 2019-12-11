9aff418ab515a1ce0b0891aea2dea013a23dfbeb
Khayyon <k_parker@coloradocollege.edu>
2019-12-11 04:53:57 -0500

    Used the addAccounts and printaccounts functions, they work :-)!!

--- src/main/java/banksy/Main.java
+++ src/main/java/banksy/Main.java
@@ -21,6 +21,8 @@ public class Main {
 */
 	public static void main(String[] args) throws IOException, SQLException {
 //	Maria_DBManager	banksyAccess = new Maria_DBManager();
+//	banksyAccess.addAccount("DEP",0);
+//	banksyAccess.printaccounts();
 	//banksyAccess.addUser("Gabriel","Morgan","gabrielmorgan1230@gmail.com","123456789","343 east 51st");
 	//	banksyAccess.printusers();
 	}
