6fd96a105293ca367b64c4dff3f09a06cdfbdb66
Khayyon <k_parker@coloradocollege.edu>
2019-12-11 10:09:13 -0500

    Added default constructor for testing purposes in Main.java and some method organization

--- src/main/java/banksy/Transaction.java
+++ src/main/java/banksy/Transaction.java
@@ -15,6 +15,11 @@ public class Transaction {
     protected Timestamp timestamp;
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
 
+    // Needed for Transaction population methods
+    public Transaction(){
+
+    }
+
     public void CreateTransaction(int userID, int accountID, int amount) {
         //Active Transactions
         //Simply creates the information for the transaction
@@ -25,10 +30,12 @@ public class Transaction {
         record.checkType();
     }
 
+    // Deposit Type Functions
     public void checkType(){//simply checks the type
         this.depositType =  this.amount > 0 ? "DEP" : "WTD" ;
     }
 
+    // Time Stamp Functions
     public void makeTimestamp(){
         //makes a timestamp
         this.timestamp = new Timestamp(System.currentTimeMillis());
@@ -36,18 +43,6 @@ public class Transaction {
 
     public void printCurrentTimestamp(){
         System.out.println(sdf.format(this.timestamp));
-    }
-
-    public boolean checkUserID(String userID){
-        //Checks if there's a valid user ID
-        //if there is, passes true then changes
-        return true;
-
-    }
-    public boolean checkAccountID(String AccountID){
-        //Checks if there's a valid account ID
-        //If passing that validation, makes the type accountID
-        return true;
     }
 
 }
