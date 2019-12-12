b8ffded7c50ca11da7f00651381a4e8781509462
Unknown <christianlazy@gmail.com>
2019-12-12 10:33:42 -0500

    Added data passing from login to dashboard.

--- web/dashboard/dashboard.js
+++ web/dashboard/dashboard.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+    var url = document.location.href,
+        params = url.split('?')[1].split('&'),
+        data = {}, tmp;
+    for (var i = 0, l = params.length; i < l; i++) {
+         tmp = params[i].split('=');
+         data[tmp[0]] = decodeURIComponent(tmp[1]);
+    }
+    for (var i = 0; i < 1; i++) {
+        $("#accounts").append("<tr><td></td><td></td><td></td></tr>");
+    }
+
     $(".container").on("click", "#deposit", function() {
         $(".container").empty().append('<label class="action"><b>Deposit</b></label>',
             '<input type="number" min="0.00" step="0.01" placeholder="Enter amount here ($)" id="amount" required>',
@@ -30,7 +41,7 @@ $(document).ready(function() {
     $(".container").on("click", "#acceptW", function() {
         var Email = $("#email").val();
         var Amount = $("#amount").val();
-        $.post('http://localhost:4567/remove', {email: Email, amount: Amount, positive: "false"}).done(function(data) {
+        $.post('http://localhost:4567/remove', {email: data.email, amount: Amount, positive: "false"}).done(function(data) {
             if (data == 'true') {
                 $('#error').text("Transaction Successful.");
             } else if (data == 'False') {
